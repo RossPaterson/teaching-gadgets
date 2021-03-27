a0c5289cc1b290b1bf75ad3585932c4b9b9ad98c
Ross Paterson <R.Paterson@city.ac.uk>
2021-03-27 10:04:22 +0000

    simpler handling of return in input elements

--- regular-expressions/src/regex_language.ts
+++ regular-expressions/src/regex_language.ts
@@ -3,23 +3,55 @@
 /// <reference path="RegExpr.ts" />
 namespace Regex {
 
+// Mapping a regular expressions to a summary of the corresponding language.
+//
+// The input field accepts basic regular expressions with alphanumeric
+// characters and meta-characters |, *, ( and ).
+
+// New interface:
+// Update the language in the target element whenever return is typed in
+// in the source input element.
+//
+// Example usage:
+// <input id="regex-id" type="text" size="15" value="" />
+// <p id="language-id"></p>
+// <script>regexAndLanguage("regex-id", "language-id");</script>
+export function regexAndLanguage(source: string, target: string): void {
+        const src_element: HTMLElement | null = document.getElementById(source);
+        const tgt_element: HTMLElement | null = document.getElementById(target);
+        if (src_element == null)
+		throw ("No " + source + " element");
+	if (! (src_element instanceof HTMLInputElement))
+		throw (source + " is not an input element")
+        if (tgt_element == null)
+		throw ("No " + target + " element");
+	src_element.onkeydown = function (e) {
+		if (e.keyCode == 13)
+			tgt_element.innerHTML =
+				languageString(src_element.value);
+	};
+}
+
+// Old interface:
 // Set content of the identified DOM element to the language denoted by
 // the regular expression text.
 //
 // Example usage:
 // <input type="text" size="15" value="" onchange="regexLanguage('target-id', this.value);"/>
 // <p id="target-id"></p>
-//
-// The input field accepts basic regular expressions with alphanumeric
-// characters and meta-characters |, *, ( and ).
 export function regexLanguage(element_id: string, re_text: string): void {
-	let text: string = "";
+	(document.getElementById(element_id) as HTMLElement).innerHTML =
+		languageString(re_text);
+}
+
+function languageString(re_text: string): string {
+	let lang_text: string = "";
 	try {
-		text = showLanguage(150, parseRegExpr(re_text));
+		lang_text = showLanguage(150, parseRegExpr(re_text));
 	} catch (err) {
-		text = `<em>Malformed expression: ${err}</em>`;
+		lang_text = `<em>Malformed expression: ${err}</em>`;
 	}
-	(document.getElementById(element_id) as HTMLElement).innerHTML = text;
+	return lang_text;
 }
 
 // String representing the language denoted by e, of length at most n (approx)
@@ -48,4 +80,5 @@ const language: (e: RegExpr) => Language = foldRegExpr({
 
 } // namespace Regex
 
+import regexAndLanguage = Regex.regexAndLanguage;
 import regexLanguage = Regex.regexLanguage;
